a09585ad6695a468306f2b2eeef94a8d1c5203ad
Kunal Gupta <kunalgupta31@hotmail.com>
2023-05-01 17:27:07 -0400

    day 1 - can get the demo

---
+++ ProductsSolution/ProductsApi/Demo/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ProductsApi.Demo;
+
+public class DemoController : ControllerBase
+{
+
+    [HttpGet("/demo")]
+    public async Task<ActionResult> GetTheDemo()
+    {
+        var response = new DemoResponse
+        {
+            Message = "Hello from the Api!",
+            CreatedAt = DateTimeOffset.Now
+        };
+        return Ok(response);
+    }
+}
---
+++ ProductsSolution/ProductsdApi.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ProductsdApi.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
