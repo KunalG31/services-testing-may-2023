f337183b8321082ad1549c9d416134609724fffc
Kunal Gupta <kunalgupta31@hotmail.com>
2023-05-02 15:30:53 -0400

    day 2 - uniqueness thing and generating slugs

--- ProductsSolution/ProductsApi/Products/SlugGenerator.cs
+++ ProductsSolution/ProductsApi/Products/SlugGenerator.cs
@@ -2,8 +2,48 @@
 
 public class SlugGenerator : IGenerateSlugs
 {
+    private readonly ICheckForUniqueValues _uniquenessChecker;
+
+    public SlugGenerator(ICheckForUniqueValues uniqueChecker)
+    {
+        _uniquenessChecker = uniqueChecker;
+     }
+
     public async Task<string> GenerateSlugForAsync(string name)
     {
-        return "blah";
+        var config = new Slugify.SlugHelperConfiguration
+        {
+            CollapseWhiteSpace = true,
+
+        };
+
+        var slugger = new Slugify.SlugHelper(config);
+        var attempt = slugger.GenerateSlug(name);
+
+        bool isUnique = await _uniquenessChecker.IsUniqueAsync(attempt);
+
+        if (isUnique)
+        {
+            return attempt;
+        }
+        else
+        {
+            var letters = "abcdefhijklmnopqrstuvwxyz";
+            var idx = 0;
+            while (idx < letters.Length)
+            {
+                var retryAttempt = attempt + "-" + letters[idx];
+                isUnique = await _uniquenessChecker.IsUniqueAsync(retryAttempt);
+                if (isUnique)
+                {
+                    return retryAttempt;
+                }
+                else
+                {
+                    idx++;
+                }
+            }
+            return attempt + Guid.NewGuid().ToString(); // discovered this.
+        }
     }
 }
